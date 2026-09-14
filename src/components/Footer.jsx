06178fd8ad9ca3dd5c177a21e3ebdb36a3dfaecf
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-09-14 15:23:12 +0530

    Updated UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-[#000000] min-h-[30vh] flex flex-col items-center justify-center gap-3">
+      <div className="flex flex-col items-center justify-center">
+        <img src="src/assets/logo.png" alt="Team Rocket Logo" className="h-15 w-auto" />
+        <p className="font-mono text-white text-xl font-bold">TEAM ROCKET</p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <p className="font-mono text-white">Prepare for trouble!</p>
+        <p className="font-mono text-white">And make it double!</p>
+      </div>
+      <p className="font-mono text-white text-xs">© 2026 Team Rocket. All rights reserved</p>
+    </footer>
   )
 }
 
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
+import { TextScramble } from '@a7sc11u/scramble'
 
 const LandingPage = () => {
     return (
-        <div  className="bg-gradient-to-r from-[#F3E6D5] via-[FFF9F2] to-[#F3E6D5]">
+        <div className="bg-gradient-to-r from-[#F3E6D5] via-[#FFF9F2] to-[#F3E6D5]">
             {/*-- HERO-SECTION --*/}
-            <div className="min-h-screen">
-                <h1>HERO SECTION</h1>
+            <div className="min-h-[85vh] flex flex-col items-center justify-center">
+                <div className="text-8xl tracking-widest uppercase text-neutral-500">
+                    <TextScramble text="INTRODUCING" as="h1" />
+                    <TextScramble text="TEAM ROCKET" as="h1" />
+                </div>
             </div>
-            {/*-- SECTION-2 --*/}
-            <div>
+            {/*-- INTRODUCTION --*/}
+            <div className="min-h-[100vh] flex flex-col items-center justify-center">
                 <h1>ABOUT/INTRO</h1>
             </div>
-            {/*-- SECTION-3 --*/}
-            <div>
+            {/*-- HIGHLIGHTS --*/}
+            <div className="min-h-[100vh] flex flex-col items-center justify-center">
                 <h1>HIGHLIGHTS</h1>
             </div>
-            {/*-- SECTION-4 --*/}
-            <div>
+            {/*-- CONTRACTS --*/}
+            <div className="min-h-[100vh] flex flex-col items-center justify-center">
                 <h1>CONTRACTS</h1>
             </div>
         </div>
