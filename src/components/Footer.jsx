94ff8dd1f6420f20ffcc29da43f4e38dee3adbdc
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-09-14 15:26:31 +0530

    Updated Logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 const Footer = () => {
   return (
     <footer className="bg-[#000000] min-h-[30vh] flex flex-col items-center justify-center gap-3">
       <div className="flex flex-col items-center justify-center">
-        <img src="../assets/logo.png" alt="Team Rocket Logo" className="h-15 w-auto" />
+        <img src={logo} alt="Team Rocket Logo" className="h-15 w-auto" />
         <p className="font-mono text-white text-xl font-bold">TEAM ROCKET</p>
       </div>
       <div className="flex flex-col items-center justify-center">
